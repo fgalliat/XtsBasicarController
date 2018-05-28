3e17ada807a8c011aedb615d685c0785f68059ba
fgalliat <fgalliat@gmail.com>
2018-05-28 13:31:36 +0200

    ADDED Telnet Bridge to XtsPocket

--- src/com/xtase/Main.java
+++ src/com/xtase/Main.java
@@ -3,31 +3,48 @@ package com.xtase;
 import com.xtase.websandbox.StaticServerManager;
 import com.xtase.websandbox.WebServerListener;
 import com.xtase.websocket.WebSocketToTelnet;
+import com.xtase.xtsubasic.MiniTelnetBridge;
 
 public class Main {
 
     protected static String currentBridge = null;
 
-    public static void connectToXtsuBasicViaWifi( String ip ) {
-        if ( currentBridge != null && currentBridge.equalsIgnoreCase(ip) ) {
+    public static void connectToXtsuBasicViaWifi(String ip) {
+        if (currentBridge != null && currentBridge.equalsIgnoreCase(ip)) {
             // notify user !
             System.out.println("########## Server already started ########");
             return;
         }
-        new Thread() { public void run() {
-        try {
-            currentBridge = ip;
-            // to Xts-uBASIC WIFI telnetd
-            WebSocketToTelnet.main(new String[]{"9878", "23", ip});
-            // notify user !
-            System.out.println("########## Server closed ########");
-            currentBridge = null;
-        } catch (Exception ex) {
-            currentBridge = null;
-            // notify user !
-            ex.printStackTrace();
-        }
-        }}.start();
+
+        new Thread() {
+            public void run() {
+                try {
+                    new MiniTelnetBridge().doWork(1123);
+                } catch (Exception ex) {
+                    currentBridge = null;
+                    // notify user !
+                    ex.printStackTrace();
+                    System.exit(0);
+                }
+            }
+        }.start();
+
+        new Thread() {
+            public void run() {
+                try {
+                    currentBridge = ip;
+                    // to telnet Bridge -> Xts-uBASIC WIFI telnetd
+                    WebSocketToTelnet.main(new String[]{"9878", "1123", ip});
+                    // notify user !
+                    System.out.println("########## Server closed ########");
+                    currentBridge = null;
+                } catch (Exception ex) {
+                    currentBridge = null;
+                    // notify user !
+                    ex.printStackTrace();
+                }
+            }
+        }.start();
     }
 
 
@@ -48,6 +65,6 @@ public class Main {
         // connectToXtsuBasicViaWifi("192.168.1.11");
         connectToXtsuBasicViaWifi("127.0.0.1");
 
-        StaticServerManager.start( args != null && args.length > 0 ? args[0] : null );
+        StaticServerManager.start(args != null && args.length > 0 ? args[0] : null);
     }
 }
---
+++ src/com/xtase/xtsubasic/MiniTelnetBridge.java
@@ -0,0 +1,166 @@
+package com.xtase.xtsubasic;
+
+import java.io.*;
+import java.util.*;
+import java.net.*;
+
+/**
+ * Telnet server that connects to Xts-Pocket daemon
+ */
+
+public class MiniTelnetBridge {
+
+    public static void main(String[] args) throws Exception {
+        new MiniTelnetBridge().doWork(23);
+    }
+
+    // ==============================
+    protected Socket espSk = null;
+    protected boolean espConn = false;
+    protected InputStream espIn = null;
+    protected OutputStream espOut = null;
+    protected String espHost = null;
+
+
+    protected boolean isPocketConnected() {
+
+        if (espSk != null && espSk.isClosed()) {
+            espConn = false;
+        }
+
+        return espConn;
+    }
+
+    protected boolean connectToPocket(String host) {
+        try {
+
+            espSk = new Socket(host, 23);
+            this.espHost = host;
+            this.espIn = espSk.getInputStream();
+            this.espOut = espSk.getOutputStream();
+            this.espConn = true;
+
+            // Greeting sequence .....
+            pocketWrite("Hello \n");
+            pocketReadline();
+
+            return true;
+        } catch (Exception ex) {
+            this.espConn = false;
+            ex.printStackTrace();
+            return false;
+        }
+    }
+
+    protected void disconnectFromPocket() {
+        if (!this.isPocketConnected()) {
+            return;
+        }
+        try {
+            this.espOut.write("/quit\n".getBytes());
+            this.espOut.flush();
+
+            this.espSk.close();
+
+            this.espConn = false;
+        } catch (Exception ex) {
+            this.espConn = false;
+        }
+    }
+
+    protected boolean pocketWrite(String str) {
+        try {
+            this.espOut.write(str.getBytes());
+            this.espOut.flush();
+
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+    protected String pocketReadline() {
+        try {
+            String str = "";
+
+            int ch;
+            while (true) {
+                ch = espIn.read();
+                if (ch == -1) {
+                    break;
+                } else if (ch == '\r') {
+                    continue;
+                } else if (ch == '\n') {
+                    break;
+                } else {
+                    str += (char) ch;
+                }
+            }
+
+            if (str.length() == 0 && ch == -1) {
+                return null;
+            }
+
+            return str;
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+
+    // ==============================
+
+
+    public void doWork(int port) throws Exception {
+        ServerSocket ssk = new ServerSocket(port);
+        _("listening on :" + port);
+
+        boolean kill = false;
+        while (!kill) {
+            Socket sk = ssk.accept();
+            _("connected on :" + port);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(sk.getInputStream()));
+            PrintStream out = new PrintStream(sk.getOutputStream());
+
+            out.println("Hi, please connect to XtsPocket");
+            String line;
+            while ((line = in.readLine()) != null) {
+                line = line.trim();
+
+                if (line.startsWith("/connect ")) {
+                    String espHost = line.trim();
+                    espHost = espHost.substring("/connect ".length()).trim();
+                    out.println("Ok, let try to connect " + espHost + ":23...");
+
+                    connectToPocket(espHost);
+
+                } else if (line.equals("/quit")) {
+                    disconnectFromPocket();
+                    // break;
+                } else if (line.equals("/kill")) {
+                    kill = true;
+                    break;
+                }
+                System.out.println("> " + line);
+
+                pocketWrite( line+"\n" );
+                String answer = pocketReadline();
+
+
+                // out.println("OK " + new Date().getSeconds());
+                out.println(">"+answer);
+            }
+
+            sk.close();
+        }
+
+        ssk.close();
+    }
+
+
+    static void _(Object o) {
+        System.out.println(o);
+    }
+
+}
+
